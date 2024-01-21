93697c6317db31d6cafc0fc9b951e6c22581de53
Jasil Shan <jasilshan786@gmail.com>
2024-01-21 12:27:48 +0530

    bug fix

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from 'cors'
-import pool from './Db/dbConfig.js';
+import productRouter from './routes/productRoutes.js'
 
 const app = express()
 
@@ -15,7 +15,7 @@ app.use(
 app.use(express.json())
 app.use(express.urlencoded({extended: true }))
 
-// app.use('/', userRouter)
+app.use('/', productRouter)
 
 app.listen(3000, () => {
     console.log(`Server listening on: 3000`);
