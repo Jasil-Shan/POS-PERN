ec848c7a8d81ee54b377439179a846bcdd3abac6
Jasil Shan <jasilshan786@gmail.com>
2024-01-21 13:57:03 +0530

    add and get offers added

--- server/controller/offerController.js
+++ server/controller/offerController.js
@@ -2,11 +2,23 @@ import pool from "../Db/dbConfig.js";
 
 export async function addOffers(req, res) {
     try {
-        const {offer_type,discount_value} = req.body
-        
-        const newOffer = await pool.query("INSERT INTO offers (offer_type,discount_value) VALUES ($1,$2) RETURNING *", [offer_type,discount_value])
+        const {offer_type,discount_value,free_product_id} = req.body
+
+        const newOffer = await pool.query("INSERT INTO offers (offer_type,discount_value,free_product_id) VALUES ($1,$2,$3) RETURNING *", [offer_type,discount_value,free_product_id])
 
         res.json(newOffer.rows[0])
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getOffers(req, res) {
+    try {
+        
+        const newOffer = await pool.query("SELECT * FROM offers")
+
+        res.json(newOffer.rows)
         
     } catch (error) {
         console.error(error);
